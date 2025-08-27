77162154b2a3e84aca22197cdc184f3a59359eb4
sion123hej <sion.0.berg@gmail.com>
2025-08-27 14:05:03 +0200

    U1 klar

---
+++ app.py
@@ -0,0 +1,17 @@
+# hospital app
+first_name = "John" #sträng /string
+last_name = "Smith" # samma ^^^
+
+age = 20 #variabel: integer / heltal
+
+is_new = False #variabel: boolean (sant eller falskt)
+
+print ("first_name, last_name, age, is_new")
+print("Förnamn" + first_name)
+print("Efternamn" + last_name)
+print("Ålder:", age )
+
+if is_new:
+    print("Ny patient")
+else:
+    print("Ny patient")
---
+++ u1 arbete/u1.py
@@ -0,0 +1,22 @@
+print("Hello world")
+print("jag ska ha med ett \"citat\" ") 
+print("""
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      Lorem ipsum
+      """)
+
+print("Hej\nHur\nMår\nDu")
+
+print("Hej", "Då")
+
+namn = input("vad heter du: ")
+
+print("Välkommen" , namn)
