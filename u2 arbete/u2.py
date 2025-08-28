25b8e3a89b99326b3d2cafb3bb4a94cca0f22c8a
sion123hej <sion.0.berg@gmail.com>
2025-08-28 15:03:09 +0200

    Hej

---
+++ u2 arbete/u2.py
@@ -0,0 +1,31 @@
+print(5+5)
+print(5-5)
+print(5*5)
+print(7//2)
+print(7/2)
+
+x = float(input("Skriv ett nummer"))
+y = float(input("Skriv ett nummber"))
+
+print(x * y)
+
+
+weight = float(input("Hur mycket väger du?"))
+length = float(input("Hur lång är du?"))
+
+print("din bmi är" , weight / length**2)
+
+age = float(input("Hur gammal är du?"))
+
+print("Du är" ,age*52, "veckor gammal")
+
+kg = float(input("Hur mycket väger du?"))
+
+print("Du väger",kg * 2.2,"lbs")
+
+print("Hej")
+
+name = str(input("Vad heter du?"))
+age = int(input("Hur gammal är du?"))     
+
+print("Välkommen",name,"Du är",age,"gammal")
