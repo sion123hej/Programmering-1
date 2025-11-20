e10d2f5c9283483a5b6923303447955a56d39e6a
sion123hej <sion.0.berg@gmail.com>
2025-11-20 20:22:24 +0100

    gissataletprojekt

--- projekt1u/gissatalet.py
+++ projekt1u/gissatalet.py
@@ -15,22 +15,30 @@ from colors import bcolors
 random.randint(1,100)
 tries = 7
 
-#cls
 print(f"{bcolors.YELLOW}Välkommen till gissa talet!")
 print(f"{bcolors.YELLOW}Du har 7 försök på dig!")
 secret_number = random.randint(1,100)
 
 while tries > 0:
     
-    print(f"{bcolors.BLUE}Vad tror du det hemliga talet kan vara?" )
-    guess = int(input())
+    print(f"{bcolors.DEFAULT}Vad tror du det hemliga talet kan vara?" )
+   
+    try:
+        guess = int(input())
+    except:
+        print(f"{bcolors.YELLOW}Du måste skriva siffor!")
+        continue
+    if guess < 1 or guess > 100:
+        print(f"{bcolors.YELLOW}Talet är mellan 1-100!")
+        continue
+
     if guess == secret_number:
         print(f"{bcolors.GREEN}Grattis! Du hittade det hemliga talet",secret_number,"med",tries-1,"försök kvar!.")
         print("Vill du fortsätta att spela? Svara Y/N: ")
         continue_playing = input()
 
         while True:
-
+            
             if continue_playing == "Y" or continue_playing == "y":
                 tries = 7
                 secret_number = random.randint(1,100)
@@ -62,13 +70,5 @@ while tries > 0:
                 print(f"{bcolors.RED}Avslutar")
                 break
             else:
-                print(f"{bcolors.YELLOW}Bara Y/N godkänns.")  
-
-   
-
-
-
-
-
-
-
+                print(f"{bcolors.YELLOW}Bara Y/N godkänns.")
+                continue
